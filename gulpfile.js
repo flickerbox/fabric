46b41e1fb05ae682dbcce71ce3ab45e8c4d0cddb
Tomasz Foster <tomaszfoster@gmail.com>
2015-04-08 17:16:59 -0700

    Added features to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,28 @@
 //
 // very important to put /*# sourceMappingURL=master.css.map */ as the first line of the master.scss
 //
-var gulp = require('gulp'),
-	uglify = require('gulp-uglify'),
-	sass = require('gulp-ruby-sass'),
-	autoprefixer = require('gulp-autoprefixer'),
-	plumber = require('gulp-plumber'),
-	notify = require('gulp-notify'),
-	concat = require('gulp-concat'),
-	sourcemaps = require('gulp-sourcemaps'),
-	livereload = require('gulp-livereload');
+// If you want live reload to work, you must install the livereload extension for chrome
+// https://chrome.google.com/webstore/detail/livereload/jnihajbhpnppcggbcgedagnkighmdlei
+//
+var gulp = 			require('gulp'),
+	uglify = 		require('gulp-uglify'),
+	sass = 			require('gulp-ruby-sass'),
+	autoprefixer = 	require('gulp-autoprefixer'),
+	plumber = 		require('gulp-plumber'),
+	notify = 		require('gulp-notify'),
+	concat = 		require('gulp-concat'),
+	sourcemaps = 	require('gulp-sourcemaps'),
+	livereload = 	require('gulp-livereload'),
+	imagemin = 		require('gulp-imagemin'),
+	jpegoptim = 		require('imagemin-jpegoptim');
+
+var srcDir = 			'src',
+	jsDestination = 	'js',
+	cssDestination = 	'css',
+	sassSource = 		'scss',
+	jsSource = 			'js',
+	imageSource = 		'src/images',
+	imageDestination = 	'images';
 
 // this is the error shown using plumber and notify:
 var onError = function(err) {
@@ -20,20 +33,20 @@ var onError = function(err) {
 
 	this.emit('end');
 };
-// Scripts Task
-// Uglifies
+
+// Uglifies a.k.a. minifies JS
 gulp.task('scripts', function() {
-	gulp.src('src/js/*.js')
+	gulp.src(srcDir+'/'+jsSource+'/*.js')
 		.pipe(plumber({errorHandler: onError}))
 		.pipe(uglify())
-		.pipe(gulp.dest('js'))
+		.pipe(gulp.dest(jsDestination))
 		.pipe(livereload()); // run livereload on js changes
 });
 
 // Styles Task
 gulp.task('styles', function() {
 	return sass (
-		'src/scss/', {
+		srcDir+'/'+sassSource+'/', {
 			style:'compressed', // this is so each created css is on its own line for sourcemapping
 			// style:'compact', // this is so each created css is on its own line for sourcemapping
 			// sourcemap: true, // this line must be here in order for...
@@ -47,35 +60,32 @@ gulp.task('styles', function() {
 	// 	// browsers: ['last 2 versions', 'ie 9'],
 	// 	cascade: false
 	// }))
-	// .pipe(gulp.dest('css'))
+	// .pipe(gulp.dest(cssDestination))
 	.pipe(sourcemaps.init())
 	.pipe(autoprefixer())
 	.pipe(concat('master.css'))
 	.pipe(sourcemaps.write('.'))
-	.pipe(gulp.dest('css'))
-	.pipe(livereload()) // run livereload on sass changes
+	.pipe(gulp.dest(cssDestination)); // run livereload on sass changes
 });
 
-gulp.task('livereload', function() {
-	livereload({start: true});
-
-	var livereloadPage = function () {
-		// Reload the whole page
-		livereload.reload();
-	};
+// this function handles image optimization
+gulp.task('images', function() {
+  return gulp.src(imageSource+'/**/*')
+    .pipe(jpegoptim({max: 70}))
+    .pipe(gulp.dest(imageDestination));
 });
+
 gulp.task('watch', function () {
-	// this writes the error message to the screen:
-	var server = livereload();
+	
+	gulp.watch(srcDir+'/'+jsSource+'/**/*.js', ['scripts']);
+	gulp.watch(srcDir+'/'+sassSource+'/*.scss', ['styles']);
+	gulp.watch(imageSource+'*.jpg', ['images']);
+	livereload.listen();
+	gulp.watch(['**/*.html','**/*.php','**/*.css'], function(event) {
+		livereload.changed(event.path);
+	}); 
 
-	gulp.watch('src/js/**/*.js', ['scripts']);
-	gulp.watch('src/scss/**/*.scss', ['styles']);
-
-	// Watch any files in current dir, reload on change
-	gulp.watch('[**/*.html,**/*.php]', function(event) {
-		var filePath = event.path.replace(/\\/g, '/');
-		livereload.changed(filePath);
-	});
 });
 
-gulp.task('default', ['scripts', 'styles', 'livereload', 'watch']);
+
+gulp.task('default', ['scripts', 'styles', 'watch', 'images']);
