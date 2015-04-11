79f2c47e5b4300764e4494f589a152543028d059
Eric Constantinides <eric@flickerbox.com>
2015-04-10 20:47:26 -0700

    Tidying up gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,31 @@
-// very important to put /*# sourceMappingURL=master.css.map */ as the first line of the master.scss
+//---------------------------------------------------------
+// Flickerbox's Gulpfile
+//---------------------------------------------------------
 //
 // If you want live reload to work, you must install the livereload extension for chrome
 // https://chrome.google.com/webstore/detail/livereload/jnihajbhpnppcggbcgedagnkighmdlei
 //
 'use strict';
 
-var gulp = 			require('gulp'),
-	uglify = 		require('gulp-uglify'),
-	sass = 			require('gulp-ruby-sass'),
-	autoprefixer = 	require('gulp-autoprefixer'),
-	plumber = 		require('gulp-plumber'),
-	notify = 		require('gulp-notify'),
-	concat = 		require('gulp-concat'),
-	sourcemaps = 	require('gulp-sourcemaps'),
-	livereload = 	require('gulp-livereload'),
-	imagemin = 		require('gulp-imagemin'),
-	jpegoptim = 	require('imagemin-jpegoptim'),
-	pngquant = 		require('imagemin-pngquant');
+var gulp =             require('gulp'),
+	uglify =           require('gulp-uglify'),
+	sass =             require('gulp-ruby-sass'),
+	autoprefixer =     require('gulp-autoprefixer'),
+	plumber =          require('gulp-plumber'),
+	notify =           require('gulp-notify'),
+	sourcemaps =       require('gulp-sourcemaps'),
+	livereload =       require('gulp-livereload'),
+	imagemin =         require('gulp-imagemin'),
+	jpegoptim =        require('imagemin-jpegoptim'),
+	pngquant =         require('imagemin-pngquant');
 
-var srcDir = 			'_src',
-	jsSource = 			'scripts',
-	jsDestination = 	'_js',
-	cssSource = 		'styles',
-	cssDestination = 	'_css',
-	imageSource = 		'_src/media',
-	imageDestination = 	'_media';
+var srcDir =           '_src',
+	jsSource =         'scripts',
+	jsDestination =    '_js',
+	cssSource =        'styles',
+	cssDestination =   '_css',
+	imageSource =      'media',
+	imageDestination = '_media';
 
 // this is the error shown using plumber and notify:
 var onError = function(err) {
@@ -36,9 +37,9 @@ var onError = function(err) {
 	this.emit('end');
 };
 
-// Uglifies a.k.a. minifies JS
+// Uglifies / minifies JS
 gulp.task('scripts', function() {
-	gulp.src(srcDir + '/' + jsSource + '/*.js')
+	gulp.src( srcDir + '/' + jsSource + '/*.js' )
 		.pipe(plumber({errorHandler: onError}))
 		.pipe(uglify())
 		.pipe(gulp.dest(jsDestination))
@@ -46,58 +47,44 @@ gulp.task('scripts', function() {
 });
 
 // Styles Task
-gulp.task('styles', function() {
-	return sass (
-		srcDir + '/' + cssSource + '/', {
-
-			// old stuff but keeping in here for reference for now
-			// style:'compressed', // this is so each created css is on its own line for sourcemapping
-			// style:'compact', // this is so each created css is on its own line for sourcemapping
-			// sourcemap: true, // this line must be here in order for...
-			// sourcemapPath: 'src/scss' // ...this line to be read
-			// however, it gives a deprecated error. It's really only used for
-			// web developer tools to know where the original files are
-
-			sourcemap: true,
-			noCache: false, 	// default = false. saves ~10% runtime but stores in a .sass-cache folder.
-			style: 'compressed'
-		}
-	)
+gulp.task('styles', function(event) {
+	return sass( srcDir + '/' + cssSource + '/', {
+		sourcemap: true,
+		noCache: false, 	// default = false. saves ~10% runtime but stores in a .sass-cache folder.
+		style: 'compressed'
+	})
 	.on('error', onError)
-	.pipe(concat('master.css'))
 	.pipe(autoprefixer())
-	.pipe(sourcemaps.write('../' + cssDestination +'/',{ 
-		sourceRoot: srcDir + '/' + cssSource,  // 
+	.pipe(sourcemaps.write('./', {
+		sourceRoot: srcDir + '/' + cssSource,  //
 		includeContent: false 		// default is true, which includes the entire css in the sourcemap
 	}))
-	.pipe(gulp.dest(cssDestination));
+	.pipe(gulp.dest(cssDestination))
+	.pipe(livereload()); // run livereload on css changes
 });
 
-// Image compression is off by default but is included. You can access by running `gulp images` 
+// Image compression is off by default but is included. You can access by running `gulp images`
 // or add it to the `default` gulp task at the bottom.
-
 gulp.task('images', function() {
-  return gulp.src(imageSource+'/**/*.{png,jpg,jpeg}')
-  	.pipe(jpegoptim({
-  		max: 70
-  	})())
-  	.pipe(pngquant({
-  		quality: 	'65-80',
-  		speed: 		4
-  		})())
-    .pipe(gulp.dest(imageDestination));
+	return gulp.src( srcDir + '/' + imageSource + '/**/*.{png,jpg,jpeg}' )
+	.pipe(jpegoptim( {
+		max: 70
+	})())
+	.pipe(pngquant( {
+		quality: '65-80',
+		speed: 4
+	})())
+	.pipe(gulp.dest(imageDestination));
 });
 
-gulp.task('watch', function () {
-
+gulp.task('watch', function() {
+	livereload.listen(); // start the livereload server
 	gulp.watch(srcDir + '/' + jsSource + '/**/*.js', ['scripts']);
-	gulp.watch(srcDir + '/' + cssSource + '/*.scss', ['styles']);
-	livereload.listen();
-	gulp.watch(['**/*.html','**/*.php', cssDestination + '/master.css' ], function(event) {
-		livereload.changed(event.path);
+	gulp.watch(srcDir + '/' + cssSource + '/**/*.scss', ['styles']);
+	gulp.watch(['**/*.{html,php,inc}'], function(event) {
+		livereload.changed(event.path); // run livereload on the file
 	});
 
 });
-
 
 gulp.task('default', ['scripts', 'styles', 'watch']);
