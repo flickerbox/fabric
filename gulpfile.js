7c92cf8e4655eead592296b240270676f3308ca2
Chris Carlevato <chris@flickerbox.com>
2016-07-22 12:53:36 -0700

    Add latest updates

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,34 @@
 'use strict';
 
 
-var gulp			= require('gulp'),
-	plugins			= require('gulp-load-plugins')()
+// look in package.json for the full list of plugins
+var node,
+	browserify 		= require('browserify'),
+	buffer 			= require('vinyl-buffer'),
+	glob 			= require('glob'),
+	gulp 			= require('gulp'),
+	plugins 		= require('gulp-load-plugins')(),
+	source 			= require('vinyl-source-stream'),
+	postcss 		= require('gulp-postcss'),
+
+// config stuff
+	_src 			= './src/',
+	_build 			= './',
+	_task_list		= ['npm:update', 'livereload:cleanup', 'livereload:start', 'lint:styles', 'compile:scripts', 'compile:styles', 'compile:oldie', 'watch']
 ;
-	
+
 
 
 // Error Messaging
-var onError = function(err) {
-	
+var onError = function(error) {
+
 	plugins.notify.onError({
-		title:    'Gulp Error',
-		message:  '<%= error.message %>',
-	})(err);
+		title: 'Gulp Error',
+		message: '<%= error.message %>',
+	})(error);
 
 	this.emit('end');
-	
+
 };
 
 
@@ -24,14 +36,15 @@ var onError = function(err) {
 // Lint SCSS
 gulp.task('lint:styles', function() {
 
-	gulp.src([
-		'src/sass/base/**/*.scss',
-		'src/sass/components/**/*.scss',
-		'src/sass/pages/**/*.scss',
-		'src/sass/master.scss'
-	]).pipe(plugins.scssLint({
-		config: 'lint.yml'
-	}));
+	return gulp
+		.src([
+			_src + 'sass/base/**/*.scss',
+			_src + 'sass/components/**/*.scss',
+			_src + 'sass/pages/**/*.scss',
+			_src + 'sass/master.scss'
+		]).pipe(plugins.scssLint({
+			config: './lint.yml'
+		}));
 
 });
 
@@ -40,11 +53,22 @@ gulp.task('lint:styles', function() {
 // Compile Scripts
 gulp.task('compile:scripts', function() {
 
-	gulp.src('src/js/**/*.js')
-		.pipe(plugins.plumber({errorHandler: onError}))
+	return browserify({
+			entries: glob.sync(_src + 'js/**/*.js'),
+			extensions: ['.js'],
+			paths: ['./node_modules', _src + 'js']
+		})
+		.bundle()
+		.on('error', onError)
+		.pipe(source('master.js'))
+		.pipe(buffer())
+		.pipe(plugins.sourcemaps.init({
+			loadMaps: true
+		}))
 		.pipe(plugins.uglify())
-		.pipe(plugins.concat('master.js'))
-		.pipe(gulp.dest('js'))
+		.on('error', onError)
+		.pipe(plugins.sourcemaps.write('./'))
+		.pipe(gulp.dest(_build + 'js'))
 		.pipe(plugins.livereload());
 
 });
@@ -54,7 +78,8 @@ gulp.task('compile:scripts', function() {
 // Compile Styles
 gulp.task('compile:styles', function() {
 
-	gulp.src('src/sass/master.scss')
+	return gulp
+		.src(_src + 'sass/master.scss')
 		.pipe(plugins.cssGlobbing({
 			extensions: ['.scss']
 		}))
@@ -63,39 +88,42 @@ gulp.task('compile:styles', function() {
 			outputStyle: 'compressed'
 		}))
 		.on('error', onError)
-		.pipe(plugins.autoprefixer())
+		.pipe(plugins.autoprefixer({
+			browsers: ['last 4 version', 'Explorer >= 8'],
+			cascade: false
+		}))
 		.pipe(plugins.sourcemaps.write('./', {
-			sourceRoot: 'src/sass',
+			sourceRoot: _src + 'sass',
 			includeContent: true
 		}))
-		.pipe(gulp.dest('css'))
+		.pipe(gulp.dest(_build + 'css'))
 		.pipe(plugins.livereload());
-	
+
 });
 
 
 
 // Watch package file for updates
 gulp.task('npm:update', function() {
-	
+
 	var update = plugins.update();
-	
+
 	update.write({
-		path: 'package.json'
+		path: './package.json'
 	});
-	
-	gulp.watch('package.json').on('change', function(file) {
-		
-		if (file.type =='changed') {
-					
+
+	plugins.watch('./package.json').on('change', function(file) {
+
+		if (file.type == 'changed') {
+
 			update.write({
-				path: 'package.json'
+				path: './package.json'
 			});
-			
+
 		}
-		
+
 	});
-	
+
 });
 
 
@@ -113,31 +141,53 @@ gulp.task('livereload:cleanup', plugins.shell.task([
 
 // Setup livereload
 gulp.task('livereload:start', function() {
-	
+
 	plugins.livereload.listen();
-	
+
 });
 
+
+
+// converts modern css to older ie compatible css
+gulp.task('compile:oldie', function () {
+    return gulp.src('./css/*.css').pipe(
+        postcss([
+            require('oldie')({ /* options */ })
+        ])
+    ).pipe(
+        gulp.dest('./css/ie')
+    );
+});
 
 
 // Watches for changes
 gulp.task('watch', function() {
-	
-	gulp.watch('src/sass/**/*.scss', ['lint:styles', 'compile:styles']);
-	gulp.watch('src/js/**/*.js', ['compile:scripts']);
-	
-	gulp.watch(['src/**/*.*']).on('change', plugins.livereload.changed);
- 	
+
+	plugins.watch(_src + 'sass/**/*.scss', function() {
+
+		gulp.run(['lint:styles', 'compile:styles']);
+
+	});
+
+	plugins.watch('./css/master.css', ['compile:oldie']);
+
+	plugins.watch(_src + 'js/**/*.js', function() {
+
+		gulp.run(['compile:scripts']);
+
+	});
+
+	plugins.watch('./**/*.php').on('change', plugins.livereload.changed);
+
 });
 
 
-
 // Initialization
-gulp.task('default', ['npm:update', 'livereload:cleanup', 'livereload:start', 'lint:styles', 'compile:scripts', 'compile:styles', 'watch']);
+gulp.task('default', _task_list);
 
 
 
 // clean up if an error goes unhandled.
 process.on('exit', function() {
-    if (node) node.kill();
+	if (node) node.kill();
 })
