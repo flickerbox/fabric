b39cc7a8fb2b536aad58e95efbf28eb881e1b186
Cody Baker <cody@flickerbox.com>
2017-11-06 10:58:39 -0800

    Update directory structure

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var path = require('path');
 
+var assets_path = path.join(__dirname, 'assets');
+var sass_path = path.join(assets_path, 'sass');
+
 module.exports = {
-  includePaths: [
-    path.join(__dirname, 'core')
-  ]
+  sassDir: sass_path
 };
